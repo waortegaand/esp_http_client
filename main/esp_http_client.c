75e858d5074f936164b435d9c4a2c0e4ccaaa30c
waortegaand <waortegaand@gmail.com>
2023-04-22 08:09:06 -0500

    spi sensor: read temperature task is added

--- main/esp_http_client.c
+++ main/esp_http_client.c
@@ -17,28 +17,47 @@
 #include "esp_event.h"
 #include "esp_netif.h"
 #include "esp_tls.h"
+#include "driver/spi_master.h"
 
 #include "esp_http_client.h"
 
 // Icludes of project
 #include "http_request.h"
 #include "wifi_connect.h" 
+#include "spi_sensor.h"
 
-static char count[40];
-static int32_t millis = 2000;
+static char msnSend[40];
+static int32_t timeSend = 2000;
+/* Sensor variables task */
+static int16_t temperature = 0;
+static uint16_t timePeriodo = 1000;
 
 static const char *TAG = "___wifi_station___";
+
+/* Task: SPI MAX6675 - Temperature Sensor Thermocouple K  */
+void temp_task(void * pvParameters) {
+  spi_device_handle_t spi = (spi_device_handle_t) pvParameters;
+  while(true)
+  {
+    temperature = read_temp(spi);
+    printf("SPI INT = %d temp=%f\n", temperature, temperature * 0.25);
+    vTaskDelay(timePeriodo / portTICK_PERIOD_MS);
+    //counter_t++;
+  }
+  vTaskDelete(NULL);
+}
+
 /* */
 static void http_test_task(void *pvParameters)
 {
 	int counter = 0;
-	while(counter<2){
+	while(counter<5){
 		counter++;
-		sprintf(count,"{\"tempTk\":\"%d\"}",counter);
-		ESP_LOGI(TAG, "Contador: %d bytes: mensaje: %s", counter, count);
-    vTaskDelay(millis / portTICK_PERIOD_MS);	
+		sprintf(msnSend,"{\"tempTk\":\"%d\"}",temperature);
+		ESP_LOGI(TAG, "Contador: %d bytes: mensaje: %s", temperature, msnSend);
+    vTaskDelay(timeSend / portTICK_PERIOD_MS);	
 		
-    http_post(count);
+    http_post(msnSend);
     http_get();
   }
     ESP_LOGI(TAG, "Finish http example");
@@ -59,6 +78,9 @@ static void http_test_task(void *pvParameters)
 
 void app_main(void)
 {
+    spi_device_handle_t spi;
+    spi = spi_init();
+
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
       ESP_ERROR_CHECK(nvs_flash_erase());
@@ -76,5 +98,6 @@ void app_main(void)
     ESP_ERROR_CHECK(example_connect());
     ESP_LOGI(TAG, "Connected to AP, begin http example");
 
+    xTaskCreate( &temp_task, "temperature_task", 4096, spi, 5, NULL );
     xTaskCreate( &http_test_task, "http_test_task", 1024*10, NULL, 5, NULL );
 }
